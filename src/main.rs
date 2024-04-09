ae2549318cb4bbf41d8812cf3d3803cd16f6237a
flashgnash <flashgnash@proton.me>
2024-04-09 16:35:02 +0100

    Numerous changes

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,125 @@
-use std::env;
+use poise::serenity_prelude as serenity;
 
-use serenity::async_trait;
-use serenity::prelude::*;
-use serenity::model::channel::Message;
-use serenity::framework::standard::macros::{command, group};
-use serenity::framework::standard::{StandardFramework, Configuration, CommandResult};
+struct Data {} // User data, which is stored and accessible in all command invocations
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
 
-#[group]
-#[commands(ping)]
-struct General;
-
-struct Handler;
-
-#[async_trait]
-impl EventHandler for Handler {}
-
-#[tokio::main]
-async fn main() {
-    let framework = StandardFramework::new().group(&GENERAL_GROUP);
-    framework.configure(Configuration::new().prefix("~")); // set the bot's prefix to "~"
-
-    // Login with a bot token from the environment
-    let token = env::var("DISCORD_TOKEN").expect("token");
-    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
-    let mut client = Client::builder(token, intents)
-        .event_handler(Handler)
-        .framework(framework)
-        .await
-        .expect("Error creating client");
-
-    // start listening for events by starting a single shard
-    if let Err(why) = client.start().await {
-        println!("An error occurred while running the client: {:?}", why);
-    }
-}
-
-#[command]
-async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "Pong!").await?;
-
+/// Displays your or another user's account creation date
+#[poise::command(slash_command, prefix_command)]
+async fn age(
+    ctx: Context<'_>,
+    #[description = "Selected user"] user: Option<serenity::User>,
+) -> Result<(), Error> {
+    let u = user.as_ref().unwrap_or_else(|| ctx.author());
+    let response = format!("{}'s account was created at {}", u.name, u.created_at());
+    ctx.say(response).await?;
     Ok(())
 }
 
+
+fn strip_non_numerical(s: &str) -> String {                                                   
+    s.chars().filter(|c| c.is_digit(10)).collect()                                    
+}    
+
+fn safe_to_number(s: &str) -> i32 {
+
+       let part_stripped = strip_non_numerical(s);
+
+        if part_stripped.len() == 0 {
+            return 0;
+        }
+
+       return part_stripped.parse::<i32>().unwrap();
+
+}
+
+#[poise::command(slash_command, prefix_command)]
+async fn roll(
+        ctx: Context<'_>,
+        dice: String
+
+    ) -> Result<(),Error> {
+
+    let instances = dice.split(' ');
+
+    let mut result: Vec<&str> = vec![0];
+
+    let mut number_of_dice = 1;
+    let mut faces_of_die = 6;
+
+    for instance in instances {
+﻿
+
+        let components : Vec<&str> = instance.split('d').collect();
+        
+
+        if components[0] == "" {
+            faces_of_die = safe_to_number(components[1]); 
+        }
+        else if components.len() == 2 {
+            faces_of_die = safe_to_number(components[1]); 
+            number_of_dice = safe_to_number(components[0]); 
+        }
+        else {
+            ctx.say("Too much D (you had more than one of the letter d in one of your rolls)").await?;
+        }
+
+        let instance_number = safe_to_number(instance);
+
+
+        if(number_of_dice == 0){
+            ctx.say("How am I supposed to roll 0 dice?").await?; //TODO handle errors elsewhere
+            return Ok(());
+        }
+        if(faces_of_die == 0){
+            ctx.say("How do you expect me to roll a d0?").await?;
+            return Ok(());
+        }
+
+        let message = format!("{} d{}s",number_of_dice,faces_of_die);
+     
+        if(number_of_dice == 1) {
+           message = format!("a d{}",faces_of_die);
+        }    
+        
+        result.push(message);
+
+    }
+
+
+    let message = result.join("\n");
+    
+
+    ctx.say(format!("Rolling... {}",message)).await?;
+        
+    Ok(())
+}
+
+
+
+#[poise::command(slash_command, prefix_command)]
+async fn ping(ctx: Context<'_>) -> Result<(),Error> {
+
+    ctx.say("Testing").await?;
+    Ok(())
+    
+}
+
+#[tokio::main]
+async fn main() {
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![age(),ping(),roll()],
+            ..Default::default()
+        })
+        .token(std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN"))
+        .intents(serenity::GatewayIntents::non_privileged())
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        });
+
+    framework.run().await.unwrap();
+}
