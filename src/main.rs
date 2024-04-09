18f3ea17ce6703061977013caaf33c18b7b0cb00
flashgnash <flashgnash@proton.me>
2024-04-09 17:26:07 +0100

    Dice roll command functional

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use poise::serenity_prelude as serenity;
-
+use rand::prelude::*;
 struct Data {} // User data, which is stored and accessible in all command invocations
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
@@ -15,6 +15,8 @@ async fn age(
     ctx.say(response).await?;
     Ok(())
 }
+
+
 
 
 fn strip_non_numerical(s: &str) -> String {                                                   
@@ -33,6 +35,45 @@ fn safe_to_number(s: &str) -> i32 {
 
 }
 
+// &[i32] is a slice reference (which means it doesn't borrow the variable)
+fn join_to_string(numbers: &[i32], separator: &str) -> String {
+    
+    let s: String = numbers.iter()                                                        
+                   .map(|i| i.to_string())                                        
+                   .collect::<Vec<String>>()                                      
+                   .join(separator);    
+    return s;
+}
+
+
+fn generate_randoms(count: i32,faces:i32) -> Vec<i32> {
+
+
+       let mut rng = rand::thread_rng();
+
+       let mut rolls : Vec<i32> = vec![];
+
+       for _i in 0..count {
+           rolls.push(rng.gen_range(1..faces));
+       }
+
+       return rolls;
+
+
+
+}
+
+fn sum_array(arr: Vec<i32>) -> i32{
+    let mut result = 0;
+
+    for num in arr {
+        result = result + num;
+
+    }
+    return result;
+}
+
+
 #[poise::command(slash_command, prefix_command)]
 async fn roll(
         ctx: Context<'_>,
@@ -42,13 +83,15 @@ async fn roll(
 
     let instances = dice.split(' ');
 
-    let mut result: Vec<&str> = vec![0];
-
-    let mut number_of_dice = 1;
-    let mut faces_of_die = 6;
+    let mut result: Vec<String> = vec![];
 
     for instance in instances {
-﻿
+
+
+        //Figure out what the user wants
+
+        let mut number_of_dice = 1;
+        let mut faces_of_die = 6;
 
         let components : Vec<&str> = instance.split('d').collect();
         
@@ -67,19 +110,32 @@ async fn roll(
         let instance_number = safe_to_number(instance);
 
 
-        if(number_of_dice == 0){
+        if number_of_dice == 0 {
             ctx.say("How am I supposed to roll 0 dice?").await?; //TODO handle errors elsewhere
             return Ok(());
         }
-        if(faces_of_die == 0){
+        if faces_of_die == 0 {
             ctx.say("How do you expect me to roll a d0?").await?;
             return Ok(());
         }
 
-        let message = format!("{} d{}s",number_of_dice,faces_of_die);
+
+        //Roll the dice
+
+
+
+
+ 
+        let dice_rolls = generate_randoms(number_of_dice, faces_of_die);
+        //Write the messages
+
+        let all_roles = join_to_string(&dice_rolls,",");
+        let total: i32 = sum_array(dice_rolls);
+
+        let mut message = format!("{} D{}s: **({})**\n[{}]",number_of_dice,faces_of_die,total,all_roles);
      
-        if(number_of_dice == 1) {
-           message = format!("a d{}",faces_of_die);
+        if number_of_dice == 1  {
+           message = format!("A D{} **({})**",faces_of_die, total);
         }    
         
         result.push(message);
@@ -87,10 +143,10 @@ async fn roll(
     }
 
 
-    let message = result.join("\n");
+    let message = result.join("\n\n");
     
 
-    ctx.say(format!("Rolling... {}",message)).await?;
+    ctx.say(format!("- Rolling...\n\n{}",message)).await?;
         
     Ok(())
 }
