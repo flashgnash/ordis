6ba235c6c3d9d533c059d6f86bc6326206d013fe
flashgnash <flashgnash@proton.me>
2024-06-03 13:32:59 +0100

    Start working on formulaic dice rolling

--- src/dice.rs
+++ src/dice.rs
@@ -12,6 +12,97 @@ use crate::common::sum_array;
 
 
 
+extern crate regex;
+use regex::Regex;
+use std::fmt;
+
+#[derive(Debug)]
+pub enum DiceError {
+    TooMuchD,
+    InvalidFaceCount,
+    InvalidDiceCount
+}
+
+impl fmt::Display for DiceError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for DiceError {}
+
+
+
+pub fn roll_one_instance(instance:&str) -> Result<(i32,Vec<i32>),DiceError> {
+
+        let mut number_of_dice = 1;
+        let mut faces_of_die = 6;
+
+        let components : Vec<&str> = instance.split('d').collect();
+        if components[0] == "" {
+            faces_of_die = safe_to_number(components[1]); 
+        }
+        else if components.len() == 2 {
+            faces_of_die = safe_to_number(components[1]); 
+            number_of_dice = safe_to_number(components[0]); 
+        }
+        else {
+            return Err(DiceError::TooMuchD);
+        }
+
+
+
+        if number_of_dice == 0 {
+            return Err(DiceError::InvalidDiceCount);
+        }
+        if faces_of_die == 0 {
+            return Err(DiceError::InvalidFaceCount);
+        }
+        if faces_of_die == 1 {
+            return Err(DiceError::InvalidFaceCount) ;
+        }
+
+        let dice_rolls = generate_randoms(number_of_dice, faces_of_die);
+        
+        Ok((sum_array(&dice_rolls),dice_rolls))
+
+}
+
+fn replace_matches(input: &str, pattern: &Regex) -> Result<(String,String), DiceError> {
+    let mut result = input.to_string();
+    let mut message = "- ";
+    for mat in pattern.find_iter(input) {
+        let (processed, rolls) = roll_one_instance(mat.as_str())?;
+       
+        let mat_str = mat.as_str()
+
+        result = result.replacen(mat.as_str(), &processed.to_string(), 1);
+        message = &format!("{message}{mat_str}: {result}, ").to_string()
+    }
+    Ok((result,message.to_string()))
+
+}
+
+fn roll_replace(text: &str) -> Result<(String,String),DiceError> { //Change name later this is terrible
+
+    let regex_string = r"\d+d\d+";
+
+
+    let regex = Regex::new(regex_string).unwrap(); // This regex pattern matches three-letter words
+
+    // let result = regex.replace_all(&original, |caps: &Captures| {
+    //     let cap = caps.get(0)?.as_str()
+    //     roll_one_instance(cap)?.to_string()
+    // });
+
+
+    let (result,message) = replace_matches(&text,&regex)?;
+
+
+    return Ok((result,message));
+
+}
+
 fn generate_randoms(count: i32,faces:i32) -> Vec<i32> {
 
 
@@ -33,6 +124,9 @@ fn generate_randoms(count: i32,faces:i32) -> Vec<i32> {
         format!("{:<width$}", input, width = total_len)                                                                                                                              
     }    
 
+
+
+
 #[poise::command(slash_command, prefix_command)]
 pub async fn roll(
         ctx: Context<'_>,
@@ -40,7 +134,14 @@ pub async fn roll(
 
     ) -> Result<(),Error> {
 
+
+    println!("{}",roll_replace(&dice)?);
+
+
+
     let instances = dice.split(' ');
+
+    
 
     let mut result: Vec<String> = vec![];
     
@@ -97,7 +198,7 @@ pub async fn roll(
         //Write the messages
 
         let all_roles = join_to_string(&dice_rolls,",");
-        let total: i32 = sum_array(dice_rolls);
+        let total: i32 = sum_array(&dice_rolls);
 
         let rolls_message = format!("- {} D{}s: **({})**",number_of_dice,faces_of_die,total);
 
