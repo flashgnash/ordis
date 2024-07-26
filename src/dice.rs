b9067810135f20c0ca40e32c909ecc8cbaed620f
flashgnash <flashgnash@proton.me>
2024-07-26 11:34:57 +0100

    Formatting and bug fixes

--- src/dice.rs
+++ src/dice.rs
@@ -1,9 +1,7 @@
-
 use rand::prelude::*;
 
-
-use crate::common::Error;
 use crate::common::Context;
+use crate::common::Error;
 
 use crate::common::safe_to_number;
 
@@ -12,18 +10,16 @@ use crate::common::sum_array;
 
 use meval::eval_str;
 
-
 extern crate regex;
 use regex::Regex;
-use std::fmt;
 use std::collections::HashMap;
-
+use std::fmt;
 
 #[derive(Debug)]
 pub enum DiceError {
     TooMuchD,
     InvalidFaceCount,
-    InvalidDiceCount
+    InvalidDiceCount,
 }
 
 impl fmt::Display for DiceError {
@@ -34,70 +30,56 @@ impl fmt::Display for DiceError {
 
 impl std::error::Error for DiceError {}
 
+pub fn roll_one_instance(instance: &str) -> Result<(i32, Vec<i32>), DiceError> {
+    let mut number_of_dice = 1;
+    let mut faces_of_die = 6;
 
+    let components: Vec<&str> = instance.split('d').collect();
+    if components[0] == "" {
+        faces_of_die = safe_to_number(components[1]);
+    } else if components.len() == 2 {
+        faces_of_die = safe_to_number(components[1]);
+        number_of_dice = safe_to_number(components[0]);
+    } else {
+        return Err(DiceError::TooMuchD);
+    }
 
-pub fn roll_one_instance(instance:&str) -> Result<(i32,Vec<i32>),DiceError> {
+    if number_of_dice == 0 {
+        return Err(DiceError::InvalidDiceCount);
+    }
+    if faces_of_die == 0 {
+        return Err(DiceError::InvalidFaceCount);
+    }
+    if faces_of_die == 1 {
+        return Err(DiceError::InvalidFaceCount);
+    }
 
-        let mut number_of_dice = 1;
-        let mut faces_of_die = 6;
+    let dice_rolls = generate_randoms(number_of_dice, faces_of_die);
 
-        let components : Vec<&str> = instance.split('d').collect();
-        if components[0] == "" {
-            faces_of_die = safe_to_number(components[1]); 
-        }
-        else if components.len() == 2 {
-            faces_of_die = safe_to_number(components[1]); 
-            number_of_dice = safe_to_number(components[0]); 
-        }
-        else {
-            return Err(DiceError::TooMuchD);
-        }
-
-
-
-        if number_of_dice == 0 {
-            return Err(DiceError::InvalidDiceCount);
-        }
-        if faces_of_die == 0 {
-            return Err(DiceError::InvalidFaceCount);
-        }
-        if faces_of_die == 1 {
-            return Err(DiceError::InvalidFaceCount) ;
-        }
-
-        let dice_rolls = generate_randoms(number_of_dice, faces_of_die);
-        
-        Ok((sum_array(&dice_rolls),dice_rolls))
-
+    Ok((sum_array(&dice_rolls), dice_rolls))
 }
 
-fn roll_matches(input: &str, pattern: &Regex) -> Result<(String,String), DiceError> {
-
-
+fn roll_matches(input: &str, pattern: &Regex) -> Result<(String, String), DiceError> {
     let mut result = input.to_string();
-    let all_rolls: HashMap<String,Vec<i32>> = HashMap::new();
+    let all_rolls: HashMap<String, Vec<i32>> = HashMap::new();
 
     let mut message = format!("- ``{input}``").to_string();
 
     for mat in pattern.find_iter(input) {
         let (processed, rolls) = roll_one_instance(mat.as_str())?;
-      
-
 
         let mat_str = mat.as_str();
 
-        message = format!("{message} {mat_str}: [{}] ",join_to_string(&rolls,","));
+        message = format!("{message} {mat_str}: [{}] ", join_to_string(&rolls, ","));
         result = result.replacen(mat_str, &processed.to_string(), 1);
-
     }
-    Ok((result,message))
-
+    Ok((result, message))
 }
 
-fn roll_replace(text: &str) -> Result<(String,String),DiceError> { //Change name later this is terrible
+fn roll_replace(text: &str) -> Result<(String, String), DiceError> {
+    //Change name later this is terrible
 
     let regex_string = r"\d+d\d+";
-
 
     let regex = Regex::new(regex_string).unwrap(); // This regex pattern matches three-letter words
 
@@ -106,86 +88,65 @@ fn roll_replace(text: &str) -> Result<(String,String),DiceError> { //Change name
     //     roll_one_instance(cap)?.to_string()
     // });
 
+    let (result, message) = roll_matches(&text, &regex)?;
 
-    let (result,message) = roll_matches(&text,&regex)?;
-
-
-    return Ok((result,message));
-
+    return Ok((result, message));
 }
 
-fn generate_randoms(count: i32,faces:i32) -> Vec<i32> {
+fn generate_randoms(count: i32, faces: i32) -> Vec<i32> {
+    let mut rng = rand::thread_rng();
 
+    let mut rolls: Vec<i32> = vec![];
 
-       let mut rng = rand::thread_rng();
+    for _i in 0..count {
+        rolls.push(rng.gen_range(1..faces));
+    }
 
-       let mut rolls : Vec<i32> = vec![];
-
-       for _i in 0..count {
-           rolls.push(rng.gen_range(1..faces));
-       }
-
-       return rolls;
-
-
-
+    return rolls;
 }
 
-    fn pad_string(input: &str, total_len: usize) -> String {                                                                                                                         
-        format!("{:<width$}", input, width = total_len)                                                                                                                              
-    }    
-
-
-
+fn pad_string(input: &str, total_len: usize) -> String {
+    format!("{:<width$}", input, width = total_len)
+}
 
 #[poise::command(slash_command, prefix_command)]
-pub async fn roll(
-        ctx: Context<'_>,
-        dice: String
-
-    ) -> Result<(),Error> {
-
-
-
-
+pub async fn roll(ctx: Context<'_>, dice: String) -> Result<(), Error> {
     let instances = dice.split(',');
 
-    
-
     let mut result: Vec<String> = vec![];
-    
-    let mut grand_total = 0.0;
 
+    let mut grand_total = 0.0;
 
     let mut longest_line = 0;
 
     for instance in instances {
+        let (replaced, messages) = roll_replace(&instance)?;
+        println!("{}", &instance);
 
+        if (&replaced != "") {
+            let calc_result = eval_str(&replaced)?;
 
-    let (replaced,messages) = roll_replace(&instance)?;
-        let calc_result = eval_str(&replaced)?;
+            let message = format!("{} = {} = __{}__", &messages, &replaced, &calc_result);
 
-        let message = format!("{} = {} = __{}__",&messages,&replaced,&calc_result);
-       
-        let total = calc_result;
-        
+            let total = calc_result;
 
-        if message.len() > longest_line {
-            longest_line = message.len();
+            if message.len() > longest_line {
+                longest_line = message.len();
+            }
+
+            grand_total = grand_total + total;
+            result.push(message);
         }
-
-        grand_total = grand_total + total;
-        result.push(message);
-
     }
 
-
     let message = result.join("\n");
-    
-    let underline = format!("__{}__",pad_string("",longest_line-8));
-    ctx.say(format!("\n**Rolling...**\n\n{}\n{}\nTotal: {}",message,underline,grand_total)).await?;
-        
+
+    let underline = format!("__{}__", pad_string("", longest_line - 8));
+    ctx.say(format!(
+        "\n**Rolling...**\n\n{}\n{}\nTotal: {}",
+        message, underline, grand_total
+    ))
+    .await?;
+
     Ok(())
 }
-
-
