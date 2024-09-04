b95260255e3e086a55c5e611482d969a6f583a69
flashgnash <flashgnash@proton.me>
2024-09-04 13:33:59 +0100

    Add build-and-run command

--- flake.nix
+++ flake.nix
@@ -6,22 +6,24 @@
 
   };
 
-  outputs = { self, nixpkgs }: 
-  let 
-    system="x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
 
-    new-migration = pkgs.writeShellScriptBin "new-migration" (''diesel migration generate $1 '');
+      new-migration = pkgs.writeShellScriptBin "new-migration" (''diesel migration generate $1 '');
 
-    redo-migration = pkgs.writeShellScriptBin "redo-migration" (''diesel migration redo'');
+      redo-migration = pkgs.writeShellScriptBin "redo-migration" (''diesel migration redo'');
 
-    run-migration = pkgs.writeShellScriptBin "run-migration" (''diesel migration run'');
+      run-migration = pkgs.writeShellScriptBin "run-migration" (''diesel migration run'');
 
-  in
-  {
-    
-    devShells.${system}.default = 
-      pkgs.mkShell {
+      build-and-debug = pkgs.writeShellScriptBin "build-and-debug" (''cargo build && lldb'');
+
+    in
+    {
+
+      devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           rustc
           cargo
@@ -30,44 +32,49 @@
           clippy
           sqlite
           sqlite-web
-          
+
           diesel-cli
           openssl.dev
-            
+
           lldb
 
           new-migration
           redo-migration
           run-migration
-        ];
 
+          build-and-debug
+        ];
 
         PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
         OUT_DIR = "./src/db";
         RUST_BACKTRACE = "full";
-    };
-
-    packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
-      pname = "Ordis";
-      version = "0.0.1";
-      src = ./.;
-  
-      cargoLock = {
-        lockFile = ./Cargo.lock;
       };
 
-      nativeBuildInputs = with pkgs; [ pkg-config openssl.dev sqlite];
-      PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "Ordis";
+        version = "0.0.1";
+        src = ./.;
 
-      # buildPhase = ''
-      #   cargo build
-      # '';
-      #
-      # installPhase = ''
-      #   mkdir -p $out/bin
-      #   cp target/debug/ordis $out/bin
-      # '';
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
 
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          openssl.dev
+          sqlite
+        ];
+        PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+
+        # buildPhase = ''
+        #   cargo build
+        # '';
+        #
+        # installPhase = ''
+        #   mkdir -p $out/bin
+        #   cp target/debug/ordis $out/bin
+        # '';
+
+      };
     };
-  };
 }
