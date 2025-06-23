052671b84681dad8f5458c367006c434a00ee9b3
flashgnash <flashgnash@proton.me>
2025-06-23 16:03:17 +0100

    Reply handler covers pings now

--- src/llm/discord/reply_handler.rs
+++ src/llm/discord/reply_handler.rs
@@ -1,9 +1,10 @@
 use async_trait::async_trait;
 use poise::serenity_prelude::EventHandler;
 
-use crate::{common::{fetch_message, fetch_message_chain}, llm};
-
-
+use crate::{
+    common::{fetch_message, fetch_message_chain},
+    llm,
+};
 
 pub struct ReplyHandler;
 
@@ -14,7 +15,6 @@ impl EventHandler for ReplyHandler {
         ctx: poise::serenity_prelude::Context,
         msg: poise::serenity_prelude::Message,
     ) {
-
         if msg.author.bot {
             return;
         }
@@ -28,22 +28,18 @@ impl EventHandler for ReplyHandler {
             if ctx.cache.current_user().id == original_message.author.id {
                 println!("LLM module Received message response");
 
-                let mut messages = vec![            
+                let mut messages = vec![llm::Message {
+                    role: llm::Role::system,
+                    content: crate::llm::Personality::Ordis.get().to_string(),
+                }];
 
-                        llm::Message {
-                            role: llm::Role::system,
-                            content: "You are Ordis, the helpful AI assistant from the game Warframe. You should take on Ordis's personality when responding to prompts, while still being helpful and accurate".to_string()
-
-                        },
-
-                    ];
-
-
-                let mut message_chain = fetch_message_chain(&ctx, channel_ref, message_ref).await.unwrap();//("Fetch message chain failed");
+                let mut message_chain = fetch_message_chain(&ctx, channel_ref, message_ref)
+                    .await
+                    .unwrap(); //("Fetch message chain failed");
                 message_chain.reverse();
 
                 for chain_message in message_chain {
-                    let role = if chain_message.author.id == ctx.cache.current_user().id  {
+                    let role = if chain_message.author.id == ctx.cache.current_user().id {
                         llm::Role::assistant
                     } else {
                         llm::Role::user
@@ -53,31 +49,45 @@ impl EventHandler for ReplyHandler {
                         role,
                         content: chain_message.content.to_string(),
                     });
-                }                   
+                }
 
                 messages.push(llm::Message {
                     role: llm::Role::assistant,
-                    content: original_message.content.to_string()
+                    content: original_message.content.to_string(),
                 });
 
                 messages.push(llm::Message {
                     role: llm::Role::assistant,
-                    content: msg.content.to_string()
+                    content: msg.content.to_string(),
                 });
 
                 for message in &messages {
-                    println!("{}",message.content)
+                    println!("{}", message.content)
                 }
 
-
-                let response = llm::generate_to_string(None,messages).await.unwrap();
-
+                let response = llm::generate_to_string(None, messages).await.unwrap();
 
                 if let Err(why) = &msg.reply(&ctx.http, response.to_string()).await {
                     println!("Error sending message: {:?}", why);
                 }
 
                 return;
+            }
+        } else if msg.mentions_user_id(ctx.cache.current_user().id) {
+            let response = crate::llm::generate_agent(
+                &msg.content,
+                None,
+                crate::llm::Personality::Ordis.get(),
+            )
+            .await
+            .expect("LLM call failed but no result in event handler");
+
+            let response_message = &response.choices[0].message.content;
+
+            println!("{}", response_message);
+
+            if let Err(why) = &msg.reply(&ctx.http, response_message.to_string()).await {
+                println!("Error sending message: {:?}", why);
             }
         }
     }
